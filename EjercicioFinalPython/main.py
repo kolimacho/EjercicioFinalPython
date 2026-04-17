2da5280ed1b663261536fa613e1bb75d57f8be2c
kolimacho <73247706+kolimacho@users.noreply.github.com>
2026-04-17 11:29:43 +0200

    Estructura del proyecto y main organizado
    Asignar tareas a cada uno dividiendo el main por partes y nombrando las variables comunes y outputs y crear estructura de archivos y carpetas

---
+++ EjercicioFinalPython/main.py
@@ -0,0 +1,21 @@
+# -------- VALIDACIONES (javier) -------- #
+def validar_dni(dni):
+def validar_telefono(tel):
+def validar_correo(correo):
+
+
+# -------- LIMPIEZA (pablo) -------- #
+def limpiar_texto(texto):
+def limpiar_telefono(tel):
+
+
+# -------- PROCESAMIENTO (iker) -------- #
+def procesar_clientes(ruta):
+
+
+# -------- GUARDADO (javier) -------- #
+def guardar_output(datos_limpios, nombre):
+
+
+# -------- MAIN (...) -------- #
+if __name__ == "__main__":
